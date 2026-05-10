32cce9fd6f5673c30c7ef547c343e6413859dda6
unknown <p.timoshev@gmail.com>
2026-05-11 02:23:11 +0300

    start learning cpp

---
+++ Yandex_handbook/Basic_of_C++/2.1/a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that";
+    std::cout << "\n  - is a better C\n  - supports data abstraction\n  - supports object-oriented programming\n  - supports generic programming.";
+
+}
